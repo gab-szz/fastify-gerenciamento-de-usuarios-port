7bffe4ee330499931bd483af0e96166a6d0eb96a
gab-szz <silvio.apad@gmail.com>
2026-01-17 15:51:24 -0400

    Adiciona testes para validação de criação e hidratação de setores

--- src/modules/setor/setor.domain.spec.ts
+++ src/modules/setor/setor.domain.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { Setor } from "./setor.domain.js";
+import { id } from "zod/locales";
 
 test("Deve criar um setor", () => {
   const nome = "Tecnologia da Informação";
@@ -7,10 +8,26 @@ test("Deve criar um setor", () => {
   expect(setor).toBeInstanceOf(Setor);
 });
 
+test("Deve dar erro ao criar um setor com ID fornecido", () => {
+  const nome = "Tecnologia da Informação";
+  expect(() => Setor.criar({ nome, id: 1 })).toThrow(
+    "Erro ao criar setor: ID não deve ser fornecido.",
+  );
+});
+
 test("Deve hidratar um setor", () => {
   const nome = "Tecnologia da Informação";
-  const dadosSetor = { nome };
+  const dadosSetor = { nome, id: 1 };
 
   const setor = Setor.hidratar(dadosSetor);
   expect(setor).toBeInstanceOf(Setor);
+});
+
+test("Deve dar erro ao hidratar setor com ID não fornecido", () => {
+  const nome = "Tecnologia da Informação";
+  const dadosSetor = { nome };
+
+  expect(() => Setor.hidratar(dadosSetor)).toThrow(
+    "Erro ao hidratar setor: ID deve ser fornecido.",
+  );
 });
