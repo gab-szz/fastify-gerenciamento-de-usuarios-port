ba1a4d4fe58b1d625203443e3c0d0ecfc9d1f7f6
gab-szz <silvio.apad@gmail.com>
2026-01-18 18:31:13 -0400

    Adiciona variáveis de ambiente para configuração do banco de dados

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,11 @@ dotenv.config();
 
 const EnvSchema = z.object({
   PORTA: z.coerce.number(),
+
+  ENDERECO_BD: z.string().min(6),
+  PORTA_BD: z.coerce.number(),
+  NOME_BD: z.string(),
+  SENHA_BD: z.string(),
 });
 
 export const env = z.parse(EnvSchema, process.env);
