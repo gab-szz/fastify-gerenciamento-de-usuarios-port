580c7af653a54e2ed0b69b71dc654d2475ec8aa6
gab-szz <silvio.apad@gmail.com>
2026-01-20 19:41:30 -0400

    adiciona o método consultarPorId à interface ISetorRepository e ajusta o retorno do método inserir para Setor

--- src/modules/setor/setor.repository.ts
+++ src/modules/setor/setor.repository.ts
@@ -5,6 +5,7 @@ import { SetorMapper } from './setor.mapper.js';
 
 export interface ISetorRepository {
   inserir(setor: Setor): Promise<SetorEntity>;
+  consultarPorId(id: number): Promise<Setor | null>;
 }
 
 export class SetorRepository implements ISetorRepository {
@@ -14,8 +15,15 @@ export class SetorRepository implements ISetorRepository {
     this.repository = repository;
   }
 
-  async inserir(setor: Setor): Promise<SetorEntity> {
+  async inserir(setor: Setor): Promise<Setor> {
     const dados = SetorMapper.domainParaEntity(setor);
-    return this.repository.save(dados);
+    return SetorMapper.entityParaDomain(
+      await this.repository.save(dados),
+    );
+  }
+
+  async consultarPorId(id: number): Promise<Setor | null> {
+    const entity = await this.repository.findOneBy({ id });
+    return entity ? SetorMapper.entityParaDomain(entity) : null;
   }
 }
