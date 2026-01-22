1a311d8ea97511f41f9dd5236dbd9dce0f5a35d6
gab-szz <silvio.apad@gmail.com>
2026-01-21 20:24:10 -0400

    adiciona métodos consultarPeloNome, atualizar e remover à interface ISetorRepository e implementação SetorRepository

--- src/modules/setor/setor.repository.ts
+++ src/modules/setor/setor.repository.ts
@@ -6,6 +6,9 @@ import { SetorMapper } from './setor.mapper.js';
 export interface ISetorRepository {
   inserir(setor: Setor): Promise<SetorEntity>;
   consultarPorId(id: number): Promise<Setor | null>;
+  consultarPeloNome(nome: string): Promise<Setor | null>;
+  atualizar(setor: Setor): Promise<Setor | null>;
+  remover(setor: Setor): Promise<boolean>;
 }
 
 export class SetorRepository implements ISetorRepository {
@@ -25,5 +28,40 @@ export class SetorRepository implements ISetorRepository {
   async consultarPorId(id: number): Promise<Setor | null> {
     const entity = await this.repository.findOneBy({ id });
     return entity ? SetorMapper.entityParaDomain(entity) : null;
+  }
+
+  async consultarPeloNome(nome: string): Promise<Setor | null> {
+    const entity = await this.repository.findOneBy({ nome });
+    return entity ? SetorMapper.entityParaDomain(entity) : null;
+  }
+
+  async atualizar(setor: Setor): Promise<Setor | null> {
+    this.validarIdExistente(setor);
+
+    const entityParaSalvar = SetorMapper.domainParaEntity(setor);
+    const entitySalva = await this.repository.save(entityParaSalvar);
+    return entitySalva
+      ? SetorMapper.entityParaDomain(entitySalva)
+      : null;
+  }
+
+  async remover(setor: Setor): Promise<boolean> {
+    this.validarIdExistente(setor);
+
+    const result = await this.repository.delete(setor.id!);
+    return result.affected ? true : false;
+  }
+
+  private async validarIdExistente(setor: Setor): Promise<void> {
+    if (!setor.id) {
+      throw new Error(
+        'Não foi informado nenhum ID para esta operação no Banco de Dados.',
+      );
+    }
+    if (!this.consultarPorId(setor.id)) {
+      throw new Error(
+        'Não encontramos nenhum setor com o ID informado para esta operação no Banco de Dados.',
+      );
+    }
   }
 }
