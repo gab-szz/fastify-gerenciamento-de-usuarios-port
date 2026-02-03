9764b18001c393d1837bc9abb17ddaa84187cd1a
gab-szz <silvio.apad@gmail.com>
2026-02-02 20:17:30 -0400

    adiciona caso de uso ConsultarSetor ao plugin de setor

--- src/modules/setor/setor.plugin.ts
+++ src/modules/setor/setor.plugin.ts
@@ -4,11 +4,13 @@ import { CriarSetorUseCase } from './use-cases/criar-setor.use-case.js';
 import { fonteDeDados } from '../../database/data-source.js';
 import { SetorEntity } from './infra/setor.entity.js';
 import { SetorRepository } from './infra/setor.repository.js';
+import { ConsultarSetorUseCases } from './use-cases/consultar-setor.use-case.js';
 
 declare module 'fastify' {
   interface FastifyRequest {
     setorUseCases: {
       criar: CriarSetorUseCase;
+      consultar: ConsultarSetorUseCases;
     };
   }
 }
@@ -18,11 +20,13 @@ export default fp(async (fastify: FastifyInstance) => {
   const setorRepository = new SetorRepository(typeormRepository);
 
   const criarSetorUseCase = new CriarSetorUseCase(setorRepository);
+  const consultarSetorUseCases = new ConsultarSetorUseCases(setorRepository);
 
   fastify.decorateRequest('setorUseCases', {
     getter() {
       return {
         criar: criarSetorUseCase,
+        consultar: consultarSetorUseCases,
       };
     },
   });
