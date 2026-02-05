12638640cd2faa0a131e45e2392e821f75e37982
gab-szz <silvio.apad@gmail.com>
2026-02-04 21:49:36 -0400

    adiciona suporte a CORS no servidor e atualiza dependências

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify, { type FastifyError } from 'fastify';
+import cors from '@fastify/cors';
 import { env } from './config/env.js';
 import {
   destruirConexaoComFonteDeDados,
@@ -9,6 +10,13 @@ import { setorRoutes } from './modules/setor/setor.routes.js';
 import { errorHandler } from './errors/error.handler.js';
 
 const app = fastify();
+
+// Configurar CORS para permitir qualquer origem (desenvolvimento)
+await app.register(cors, {
+  origin: true, // Permite qualquer origem
+  credentials: true, // Permite envio de credenciais
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos HTTP permitidos
+});
 
 await inicializarConexaoComFonteDeDados();
 
