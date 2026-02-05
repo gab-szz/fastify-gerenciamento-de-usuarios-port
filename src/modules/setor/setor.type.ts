2f5590a374093e8e90a7fa2f2233fb8592fd58f8
gab-szz <silvio.apad@gmail.com>
2026-02-04 20:24:44 -0400

    adiciona schemas para consulta e atualização de setores

--- src/modules/setor/setor.type.ts
+++ src/modules/setor/setor.type.ts
@@ -12,5 +12,19 @@ export const criarSetorSchema = z.object({
   nome: z.string().min(4),
 });
 
+export const consultarSetorPorNomeSchema = z.object({
+  nome: z.string().min(3),
+});
+
+export const consultarSetorPorIdSchema = z.object({
+  id: z.coerce.number().min(1),
+});
+
+export const atualizarSetorSchema = z.object({
+  id: z.coerce.number().min(1),
+  nome: z.string().min(4),
+});
+
 export type setorType = z.infer<typeof setorSchema>;
 export type criarSetorDTO = z.infer<typeof criarSetorSchema>;
+export type atualizarSetorDTO = z.infer<typeof atualizarSetorSchema>;
