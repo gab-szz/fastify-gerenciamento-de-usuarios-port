e5b818397a1fc160f10e9223086568c4ebac4aad
gab-szz <silvio.apad@gmail.com>
2026-02-04 20:24:41 -0400

    refatora SetorController para incluir métodos de consulta, atualização e exclusão de setores

--- src/modules/setor/setor.controller.ts
+++ src/modules/setor/setor.controller.ts
@@ -1,5 +1,10 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
-import { criarSetorSchema } from './setor.type.js';
+import {
+  atualizarSetorSchema,
+  consultarSetorPorIdSchema,
+  consultarSetorPorNomeSchema,
+  criarSetorSchema,
+} from './setor.type.js';
 import { ZodError } from 'zod';
 
 export class SetorController {
@@ -14,5 +19,29 @@ export class SetorController {
   async consultar(request: FastifyRequest, reply: FastifyReply) {
     const setores = await request.setorUseCases.consultar.todos();
     return reply.status(200).send(setores);
+  }
+
+  async consultarPorNome(request: FastifyRequest, reply: FastifyReply) {
+    const input = consultarSetorPorNomeSchema.parse(request.params);
+    const setores = await request.setorUseCases.consultar.porNome(input.nome);
+    return reply.status(200).send(setores);
+  }
+
+  async consultarPorId(request: FastifyRequest, reply: FastifyReply) {
+    const input = consultarSetorPorIdSchema.parse(request.params);
+    const setor = await request.setorUseCases.consultar.porId(input.id);
+    return reply.status(200).send(setor);
+  }
+
+  async atualizar(request: FastifyRequest, reply: FastifyReply) {
+    const input = atualizarSetorSchema.parse(request.query);
+    const setor = await request.setorUseCases.atualizar.executar(input);
+    return reply.status(200).send(setor);
+  }
+
+  async excluir(request: FastifyRequest, reply: FastifyReply) {
+    const input = consultarSetorPorIdSchema.parse(request.query);
+    const setor = await request.setorUseCases.excluir.executar(input.id);
+    return reply.status(200).send(setor);
   }
 }
