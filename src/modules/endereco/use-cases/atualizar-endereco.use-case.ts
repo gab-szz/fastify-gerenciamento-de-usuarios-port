364624de454f518994768d469fbc6629306a1343
gab-szz <silvio.apad@gmail.com>
2026-03-10 22:09:25 -0400

    adiciona caso de uso para atualizar endereço com validação de existência

---
+++ src/modules/endereco/use-cases/atualizar-endereco.use-case.ts
@@ -0,0 +1,20 @@
+import type { Endereco } from '../domain/endereco.domain.js';
+import type { atualizarEnderecoDTO } from '../endereco.type.js';
+import type { EnderecoRepository } from '../infra/endereco.repository.js';
+
+export class AtualizarEnderecoUseCase {
+  constructor(private readonly repository: EnderecoRepository) {}
+
+  async executar(input: atualizarEnderecoDTO) {
+    const endereco = await this.validarSeEnderecoExiste(input.id);
+    return await this.repository.atualizar(endereco.atualizar(input));
+  }
+
+  private async validarSeEnderecoExiste(id: number): Promise<Endereco> {
+    const endereco = await this.repository.consultarPorId(id);
+    if (!endereco) {
+      throw new Error('Not Exists');
+    }
+    return endereco;
+  }
+}
