38a8bd4f8155c8364604fc46cb7b3241eb3d40d4
gab-szz <silvio.apad@gmail.com>
2026-03-11 21:10:23 -0400

    adiciona métodos para atualizar e excluir endereços no controlador

--- src/modules/endereco/endereco.controller.ts
+++ src/modules/endereco/endereco.controller.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import {
+  atualizarEnderecoSchema,
   consultarEnderecoPorIdSchema,
   criarEnderecoSchema,
 } from './endereco.type.js';
@@ -26,5 +27,23 @@ export class EnderecoController {
   async consultarTodos(request: FastifyRequest, reply: FastifyReply) {
     const enderecos = await request.enderecoUseCases.consultar.todos();
     return reply.send(enderecos);
+  }
+
+  async atualizar(request: FastifyRequest, reply: FastifyReply) {
+    const input = atualizarEnderecoSchema.parse(request.body);
+    const endereco = await request.enderecoUseCases.atualizar.executar(input);
+    if (!endereco) {
+      return reply.status(404).send({ message: 'Endereço não encontrado' });
+    }
+    return reply.send(endereco);
+  }
+
+  async excluir(request: FastifyRequest, reply: FastifyReply) {
+    const input = consultarEnderecoPorIdSchema.parse(request.params);
+    const sucesso = await request.enderecoUseCases.excluir.executar(input.id);
+    if (!sucesso) {
+      return reply.status(404).send({ message: 'Endereço não encontrado' });
+    }
+    return reply.status(204).send();
   }
 }
