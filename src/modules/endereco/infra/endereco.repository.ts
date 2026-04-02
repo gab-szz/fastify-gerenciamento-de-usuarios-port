1e56623659add85b50a41254d5bb8cb9ca3dcb35
gab-szz <silvio.apad@gmail.com>
2026-04-02 12:48:04 -0400

    adiciona documentação para os métodos do EnderecoRepository

--- src/modules/endereco/infra/endereco.repository.ts
+++ src/modules/endereco/infra/endereco.repository.ts
@@ -14,6 +14,11 @@ export interface IEnderecoRepository {
 export class EnderecoRepository {
   constructor(private readonly repository: Repository<EnderecoEntity>) {}
 
+  /**
+   * Cria um novo setor no Banco de Dados
+   * @Param endereco - Objeto do tipo Endereco a ser convertido a entidade e inserido no banco de dados
+   * @Returns Objeto do tipo Endereco criado
+   */
   async inserir(endereco: Endereco): Promise<Endereco> {
     const dados = EnderecoMapper.domainParaEntity(endereco);
     return EnderecoMapper.entityParaDomain(
@@ -21,6 +26,10 @@ export class EnderecoRepository {
     );
   }
 
+  /**
+   * Consulta todos os endereços no Banco de Dados
+   * @Returns Array de objetos do tipo Endereco com todos os endereços cadastrados
+   */
   async consultarTodos(): Promise<Endereco[]> {
     const entities = await this.repository.find();
     let enderecos: Endereco[] = [];
@@ -32,11 +41,21 @@ export class EnderecoRepository {
     return enderecos;
   }
 
+  /**
+   * Consulta um endereço específico pelo ID no Banco de Dados
+   * @Param id - Identificador único do endereço
+   * @Returns Objeto do tipo Endereco ou null se não encontrado
+   */
   async consultarPorId(id: number): Promise<Endereco | null> {
     const entity = await this.repository.findOneBy({ id });
     return entity ? EnderecoMapper.entityParaDomain(entity) : null;
   }
 
+  /**
+   * Atualiza um endereço existente no Banco de Dados
+   * @Param endereco - Objeto do tipo Endereco a ser atualizado
+   * @Returns Objeto do tipo Endereco atualizado ou null se não encontrado
+   */
   async atualizar(endereco: Endereco): Promise<Endereco | null> {
     const entitySalva = await this.repository.save(
       EnderecoMapper.domainParaEntity(endereco),
@@ -44,6 +63,11 @@ export class EnderecoRepository {
     return entitySalva ? EnderecoMapper.entityParaDomain(entitySalva) : null;
   }
 
+  /**
+   * Remove um endereço do Banco de Dados
+   * @Param endereco - Objeto do tipo Endereco a ser removido
+   * @Returns Boolean indicando se a remoção foi bem-sucedida
+   */
   async remover(endereco: Endereco): Promise<boolean> {
     const resultado = await this.repository.delete(endereco.id!);
     return resultado.affected ? true : false;
