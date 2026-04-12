5efd483a33caa8059693fc1a20d05819a1dce52f
gab-szz <silvio.apad@gmail.com>
2026-04-11 21:06:58 -0400

    adiciona caso de uso para atualização de perfil com verificação de existência

---
+++ src/modules/perfil/use-cases/atualizar-perfil.use-case.ts
@@ -0,0 +1,22 @@
+import type { Perfil } from '../domain/perfil.domain.js';
+import type { atualizarPerfilDTO } from '../dtos/atualizar-perfil.dto.js';
+import type { IPerfilRepository } from '../infra/perfil.repository.js';
+
+export class AtualizarPerfilUseCase {
+  constructor(private readonly rep: IPerfilRepository) {}
+
+  async exec(input: atualizarPerfilDTO): Promise<Perfil | null> {
+    let perfil = await this.rep.consultarPorId(input.id);
+    perfil = this.verificarSePerfilExiste(perfil, input.id);
+
+    perfil.atualizar(input);
+    return this.rep.atualizar(perfil);
+  }
+
+  private verificarSePerfilExiste(perfil: Perfil | null, id: number) {
+    if (!perfil) {
+      throw new Error(`Perfil com id ${id} não encontrado.`);
+    }
+    return perfil;
+  }
+}
