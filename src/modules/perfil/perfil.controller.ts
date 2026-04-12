1142a017c0c6ec5ed23272ab117811734bb3edea
gab-szz <silvio.apad@gmail.com>
2026-04-11 21:30:12 -0400

    implementa método de criação de perfil no PerfilController

--- src/modules/perfil/perfil.controller.ts
+++ src/modules/perfil/perfil.controller.ts
@@ -1,7 +1,13 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
+import z from 'zod';
+import { criarPerfilSchema } from './dtos/criar-perfil.dto.js';
 
 export class PerfilController {
   constructor() {}
 
-  async criar(req: FastifyRequest, rep: FastifyReply) {}
+  async criar(request: FastifyRequest, reply: FastifyReply) {
+    const input = z.parse(criarPerfilSchema, request.body);
+    const perfil = await request.perfilUseCases.criar.exec(input);
+    return reply.status(201).send(perfil);
+  }
 }
