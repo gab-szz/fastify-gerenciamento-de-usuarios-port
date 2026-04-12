92e8f06d1877a3b958980ac3b435d6bf40a2db0d
gab-szz <silvio.apad@gmail.com>
2026-04-11 21:30:03 -0400

    refatora rotas de perfil para utilizar PerfilController

--- src/modules/perfil/perfil.routes.ts
+++ src/modules/perfil/perfil.routes.ts
@@ -1,33 +1,8 @@
-import fp from 'fastify-plugin';
-import { type FastifyInstance } from 'fastify';
-import { fonteDeDados } from '../../database/data-source.js';
-import { CriarPerfilUseCase } from './use-cases/criar-perfil.use-case.js';
-import { AtualizarPerfilUseCase } from './use-cases/atualizar-perfil.use-case.js';
-import { PerfilEntity } from './infra/perfil.entity.js';
-import { PerfilRepository } from './infra/perfil.repository.js';
+import type { FastifyInstance } from 'fastify';
+import { PerfilController } from './perfil.controller.js';
 
-declare module 'fastify' {
-  interface FastifyRequest {
-    perfilUseCases: {
-      criar: CriarPerfilUseCase;
-      atualizar: AtualizarPerfilUseCase;
-    };
-  }
+export async function perfilRoutes(fastify: FastifyInstance) {
+  const controller = new PerfilController();
+
+  fastify.post('/perfil', controller.criar.bind(controller));
 }
-
-export default fp(async (fastify: FastifyInstance) => {
-  const typeormRepository = fonteDeDados.getRepository(PerfilEntity);
-  const perfilRepository = new PerfilRepository(typeormRepository);
-
-  const criarPerfilUseCase = new CriarPerfilUseCase(perfilRepository);
-  const atualizarPerfilUseCase = new AtualizarPerfilUseCase(perfilRepository);
-
-  fastify.decorateRequest('perfilUseCases', {
-    getter() {
-      return {
-        criar: criarPerfilUseCase,
-        atualizar: atualizarPerfilUseCase,
-      };
-    },
-  });
-});
