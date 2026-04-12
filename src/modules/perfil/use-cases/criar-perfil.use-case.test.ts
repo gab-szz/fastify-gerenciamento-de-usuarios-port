6736be16eda220f326183ea555c59f8bae742c2f
gab-szz <silvio.apad@gmail.com>
2026-04-11 21:07:03 -0400

    adiciona testes para o caso de uso de criação de perfil

---
+++ src/modules/perfil/use-cases/criar-perfil.use-case.test.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it, vi, type Mock } from 'vitest';
+import type { IPerfilRepository } from '../infra/perfil.repository.js';
+import { Perfil } from '../domain/perfil.domain.js';
+import { CriarPerfilUseCase } from './criar-perfil.use-case.js';
+import { number } from 'zod';
+
+describe('CriarPerfilUseCase', () => {
+  let mockRepository: IPerfilRepository;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    mockRepository = {
+      consultarPorId: vi.fn(),
+      consultarPorNome: vi.fn(),
+      consultarTodos: vi.fn(),
+      inserir: vi.fn(),
+      atualizar: vi.fn(),
+      remover: vi.fn(),
+    };
+  });
+
+  it('Deve criar um perfil com sucesso', async () => {
+    // ARRANGE
+    const input = { nome: 'T.I.' };
+    const useCase = new CriarPerfilUseCase(mockRepository);
+
+    (mockRepository.inserir as Mock).mockResolvedValue(
+      Perfil.hidratar({ id: 1, nome: input.nome, criadoEm: new Date() }),
+    );
+
+    // ACT
+    const perfil = await useCase.exec(input);
+
+    // ASSERT
+    expect(perfil).toBeInstanceOf(Perfil);
+    expect(perfil!.id!).toBe(1);
+    expect(perfil!.nome).toBe('T.I.');
+    expect(perfil?.criadoEm).toBeInstanceOf(Date);
+    expect(mockRepository.inserir).toHaveBeenCalledOnce();
+  });
+});
