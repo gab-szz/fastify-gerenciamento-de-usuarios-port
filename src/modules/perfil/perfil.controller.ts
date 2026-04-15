cbe1226e5b233ef63d402f9a1245ef2f8a366361
gab-szz <silvio.apad@gmail.com>
2026-04-14 20:10:13 -0400

    adiciona documentação para o método de criação de perfil na classe PerfilController

--- src/modules/perfil/perfil.controller.ts
+++ src/modules/perfil/perfil.controller.ts
@@ -5,6 +5,12 @@ import { criarPerfilSchema } from './dtos/criar-perfil.dto.js';
 export class PerfilController {
   constructor() {}
 
+  /**
+   * Cria um novo perfil.
+   * @param request Requisição recebida
+   * @param reply Retorno da requisição
+   * @returns Perfil criado
+   */
   async criar(request: FastifyRequest, reply: FastifyReply) {
     const input = z.parse(criarPerfilSchema, request.body);
     const perfil = await request.perfilUseCases.criar.exec(input);
