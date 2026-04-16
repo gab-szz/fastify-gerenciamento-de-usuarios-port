f2f75b8ee3fb69559f573fd15dc51365592264a2
gab-szz <silvio.apad@gmail.com>
2026-04-15 20:55:35 -0400

    refatora estrutura do projeto removendo routes de Endereco, Perfil e Setor

--- src/modules/perfil/perfil.routes.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import { PerfilController } from './perfil.controller.js';
-
-export async function perfilRoutes(fastify: FastifyInstance) {
-  const controller = new PerfilController();
-
-  fastify.post('/perfil', controller.criar.bind(controller));
-}
